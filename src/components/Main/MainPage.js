67538cfdbd82946a34cc1932d985f67105da3e5c
sanliverpool13 <kairosh.sanjar@gmail.com>
2020-04-15 21:46:34 -0400

    finished layout

--- src/components/Main/MainPage.js
+++ src/components/Main/MainPage.js
@@ -1,39 +1,63 @@
 import React from 'react';
-import { Grid, Card, CardHeader, CardContent, CardActionArea, Typography } from "@material-ui/core";
+import { Grid, Card, CardHeader, CardContent, Paper, InputBase, 
+     CardActionArea, Typography, IconButton } from "@material-ui/core";
+import { Search } from "@material-ui/icons";
 import styles from './main.module.css';
 import cx from 'classnames';
+// import useStyles from './main.css';
 
 const Main = () => {
-     
+
      return (
           <Grid container className={styles.gridContainer}>
                <Grid item xs={12} className={styles.gridItem}>
-                    <Typography variant="h2" component="h2">
-                         COVID-19 DATA
-                    </Typography>
-               </Grid>
-               <Grid item xs={12} >
-                    <Card >
-                         <CardHeader>
-                              <Typography>
-                                   Confirmed
+                    <Card className={styles.card} raised>
+                         <CardHeader
+                              title={
+                                   <Typography variant="h5"  className={styles.cardTitle}>
+                                        Confirmed
+                                   </Typography>
+                              }
+                              className={styles.cardHeader}
+                         />
+                         <CardContent className={styles.cardContent}>
+                              <Typography variant="h3">
+                                   2,000,000
                               </Typography>
-                         </CardHeader>
-                         
+                         </CardContent>
                     </Card>
-                    <Card >
-                         <Typography>
-                              Recovered
-                         </Typography>
+                    <Card className={styles.card} raised>
+                         <CardHeader 
+                              title={
+                                   <Typography variant="h5"  className={styles.cardTitle}>
+                                        Recovered
+                                   </Typography>
+                              }
+                              className={styles.cardHeader}
+                         />
                     </Card>
-                    <Card >
-                         <Typography>
-                              Deaths
-                         </Typography>
+                    <Card className={styles.card} raised>
+                         <CardHeader 
+                              title={
+                                   <Typography variant="h5"  className={styles.cardTitle}>
+                                        Deaths
+                                   </Typography>
+                              }
+                              className={styles.cardHeader}
+                         />
                     </Card>
                </Grid>
-               <Grid item xs={12} className={styles.gridItem}>
-
+               <Grid item xs={12} className={styles.gridItem2}>
+                    <Paper component="form" className={styles.searchInput}>
+                         <InputBase
+                              placeholder="Search Country"
+                              inputProps={{'aria-label': 'search countries'}}
+                              className={styles.searchInputBase}
+                         />
+                         <IconButton>
+                              <Search/>
+                         </IconButton>
+                    </Paper>
                </Grid>
           </Grid>
           
---
+++ src/components/Main/main.css.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    cardTitle: {
+        
+        border:'1px solid black'
+    },
+});
+
+export default useStyles;
